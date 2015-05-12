5b743bbfe937cdb636c92492e0373f8a9fa4334d
carlo <carlo.laviola@gmail.com>
2015-05-12 12:11:28 +0200

    RigaOrdine aggiunta

---
+++ src/dati/Ordine.java
@@ -0,0 +1,14 @@
+package dati;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Ordine {
+	
+	private List<RigaOrdine> righeOrdine = new ArrayList<RigaOrdine>();
+	
+	private Date dataApertura;
+	
+
+}
---
+++ src/dati/RigaOrdine.java
@@ -0,0 +1,46 @@
+package dati;
+
+public class RigaOrdine {
+	
+	private Prodotto prod;
+	
+	private int quantity;
+	
+	private float costoUnitario;
+	
+	private float costo;
+	
+	public RigaOrdine(Prodotto prod, int quantity){
+		this.prod=prod;
+		this.quantity=quantity;
+		this.costoUnitario=prod.getCosto();
+		this.costo=this.costoUnitario*this.quantity;		
+	}
+
+	public Prodotto getProd() {
+		return prod;
+	}
+
+	public void setProd(Prodotto prod) {
+		this.prod = prod;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public float getCosto() {
+		return costo;
+	}
+
+	public void setCosto(float costo) {
+		this.costo = costo;
+	}
+	
+	
+
+}
